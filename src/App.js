2705b6abf333ad2b041b4c2a2dac0d6be002dd5b
TX-9 <jacobleecs@gmail.com>
2020-02-22 18:25:24 -0500

    dynamic css change

--- src/App.js
+++ src/App.js
@@ -46,7 +46,8 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
@@ -69,11 +70,20 @@ class App extends Component {
                         })
                     }
                 </div>
-            )
+            );
+            style.backgroundColor = 'red';
+        }
+        const classes = [];
+        if (this.state.persons.length <=2) {
+            classes.push('blue');
+        }
+        if (this.state.persons.length <=1) {
+            classes.push('bold');
         }
         return (
             <div className="App">
                 <h1>My React</h1>
+                <p className={classes.join(' ')}>css test</p>
                 <button
                     style={style}
                     onClick={this.togglePersonsHandler}>Toggle Persons</button>
