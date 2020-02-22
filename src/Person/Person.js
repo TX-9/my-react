fc356e58958783d8d17dbb018034ad7bdf5c27e6
TX-9 <jacobleecs@gmail.com>
2020-02-22 06:46:51 -0500

    add dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const person = () => {
-    return <p>1st Person</p>
+    return <p>1st Person and my address is {Math.floor(Math.random()*20)} Quest Street</p>
 };
 
 export default person;
